23f3e18613925782c86be7c3b4834beded627018
rimi <yerimi@kakao.com>
2020-12-28 17:08:10 +0900

    1일차 : Response-entity

--- src/main/kotlin/com/example/mvc/controller/response/ResponseApiController.kt
+++ src/main/kotlin/com/example/mvc/controller/response/ResponseApiController.kt
@@ -1,6 +1,7 @@
 package com.example.mvc.controller.response
 
 import com.example.mvc.model.http.UserRequest
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
@@ -16,12 +17,12 @@ class ResponseApiController {
 
         return age?.let {
             // age not null
-            if(age < 20){
+            if (age < 20) {
                 return ResponseEntity.status(400).body("age 값은 20보다 커야합니다.")
             }
 
             ResponseEntity.ok("OK")
-        }?: kotlin.run {
+        } ?: kotlin.run {
             // age is null
             return ResponseEntity.status(400).body("age 값이 누락되었습니다.")
         }
@@ -44,10 +45,19 @@ class ResponseApiController {
     // 2. post 200
     @PostMapping("")
     fun postMapping(@RequestBody userRequest: UserRequest?): ResponseEntity<Any> {
-        return ResponseEntity.status(200).body(userRequest)
+        return ResponseEntity.status(200).body(userRequest)     // object mapper -> object -> json
     }
 
     // 3. put 201
+    @PutMapping("")
+    fun putMapping(@RequestBody userRequest: UserRequest?): ResponseEntity<UserRequest> {
+        // 1. 기존 데이터가 없어서 새로 생성했다.
+        return ResponseEntity.status(HttpStatus.CREATED).body(userRequest)
+    }
 
     // 4. delete 500
+    @DeleteMapping("/{id}")
+    fun deleteMapping(@PathVariable id: Int): ResponseEntity<Any> {
+        return ResponseEntity.status(500).body(null)
+    }
 }
